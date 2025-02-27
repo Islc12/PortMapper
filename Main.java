9436255d92e9e055637bcb24db93f45f0a6e0297
Rich Smith <richrsmith@proton.me>
2025-02-26 23:26:34 -0500

    Began development on the stage, created a fixed size for the stage, with the exception of 720p monitors it should be a good size for most modern monitors. Took away resizing until I know if the table will automatically adjust to resizing, that node will be a ways out. Also added my brand logo for funzies

--- Main.java
+++ Main.java
@@ -12,7 +12,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 // Scene and Nodes (For handling the graphical interface)
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 // Event Handling (For detecting button presses)
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -27,8 +29,26 @@ import javafx.concurrent.Task;
 // REMINDER USE THIS TO RUN
 // java --module-path lib --add-modules javafx.controls,javafx.fxml portmapper
 
-public class portmapper {
+public class Main extends Application {
+    
     public static void main(String[] args) {
-        System.out.print("Hello");
+        Application.launch(args);
+    }
+
+    public void start(Stage mainStage) throws Exception {
+        
+        Group root = new Group(); // Creates root node to run the scene
+        Scene scene = new Scene(root,Color.web("272727")); // Using Color.web() allows you to pass a hex value for a color as an arg rather than Color.<COLOR> otherwise
+        mainStage.setTitle("Port Mapper v1.0");
+        mainStage.setWidth(1800); // Comfortable stage width without dealing with the stage hanging off the edge of the screen
+        mainStage.setHeight(1000);
+        mainStage.setResizable(false); // Prevents user resizing, may be able to change after I see how the table works out
+        mainStage.setX(60); // Centers the stage by default in the center of my screen
+        Image icon = new Image("Extra/arrow_s.png");
+        mainStage.getIcons().add(icon);
+        
+
+        mainStage.setScene(scene);
+        mainStage.show();
     }
 }
